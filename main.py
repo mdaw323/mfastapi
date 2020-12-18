41b714a6f17ea8eeaf70e1761bfe2dd416e00b60
mdaw <mdaw@protonmail.com>
2020-12-18 18:15:00 +0100

    add system info to response

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from datetime import datetime
+import platform
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World", "datetime": datetime.now()}
+
+    return {"message": "Hello World",
+            "datetime": datetime.now(),
+            "system": platform.version()
+            }
